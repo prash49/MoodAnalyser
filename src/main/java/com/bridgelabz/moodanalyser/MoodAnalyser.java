516d89ac243ef5d391b66f426cd4769dcb6a75c1
Prashanth <imprash4u@gmail.com>
2021-09-17 18:19:50 +0530

    Added : Welcome message

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.moodanalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class MoodAnalyser
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome To Mood analyser" );
+    }
+}
---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.moodanalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MoodAnalyserTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
